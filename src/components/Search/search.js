8e11ba8854e06c6229011edc69b14a98b2b5a919
jdrabot <jdrabot@hotmail.com>
2021-07-10 23:31:59 -0400

    continuing to work on components

--- src/components/Search/search.js
+++ src/components/Search/search.js
@@ -1,10 +1,27 @@
+import { checkPropTypes } from "prop-types";
 import React from "react";
 import "./style.css";
 
-function Search(props) {
-    return {
-
-    }
+function Search() {
+    return (
+        <form>
+            <div className="form-group">
+                <input type="text" onChange={props.handleInputChange}
+                    value={props.search} name="search" className="form-control"
+                    placeholder="Search by first or last name" id="search" />
+            </div>
+            <div className="container">
+                <div className="center">
+                    <button onClick={props.handleFormSubmit}
+                        className="btn btn-primary mt-3" id="search">
+                        SEARCH
+                    </button>
+                </div>
+            </div>
+        </form>
+    );
 }
+
+export default Search;
 
 export default Search;
